f8bb5f6a23fab1618d0aeff7192557ddc4bd7456
ovbagchi <ovsona@gmail.com>
2020-06-16 21:06:51 +0530

    Introduced MobX State Management into the React Component

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,23 +1,19 @@
-import React, { useState,  FormEvent } from 'react'
+import React, { useState,  FormEvent, useContext } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import {v4 as uuid} from 'uuid'
+import ActivityStore from '../../../app/stores/activityStore'
+import { observer } from 'mobx-react-lite'
 
 interface IProps {
-    setEditMode: (editMode: boolean) => void;
     activity: IActivity;
-    editActivity: (activity: IActivity) => void;
-    createActivity: (activity: IActivity) => void;
-    submitting:boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({ 
-    setEditMode, 
-    activity: initialFormState ,
-    editActivity,
-    createActivity,
-    submitting
+    activity: initialFormState
 }) => {
+    const activityStore = useContext(ActivityStore);
+    const {createActivity,editActivity,submitting,cancelFormOpen} =activityStore ;
 
     const initializeForm = () => {
         if (initialFormState) {
@@ -64,10 +60,10 @@ const ActivityForm: React.FC<IProps> = ({
                 <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
                 <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
                 <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-                <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
+                <Button onClick={cancelFormOpen} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
 }
 
-export default ActivityForm
+export default observer(ActivityForm);
